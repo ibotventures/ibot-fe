6795405bdf3f4131b7ec23417918424960aa82ba
Aalia-Fathima <aaliafathima14@gmail.com>
2024-12-25 16:14:02 +0530

    25 dec 2024

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -1,14 +1,14 @@
 // pages/_document.js
-
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          {/* Add the Razorpay script here */}
-          <script
+          {/* Use next/script to load Razorpay */}
+          <Script
             src="https://checkout.razorpay.com/v1/checkout.js"
             strategy="beforeInteractive" // Ensure it loads before the page is interactive
           />
